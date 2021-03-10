3edb136914ff67af1fa39a092edda6d7166f66ad
buranxiangsi <l13789060033@163.com>
2021-03-10 09:26:17 +0800

    3 链式操作

--- src/main.js
+++ src/main.js
@@ -1,8 +1,8 @@
-const api = jQuery('.test')//不返回元素们，返回aip对象
+jQuery('.test')//不返回元素们，返回aip对象
 
-// 遍历所有刚才获取的元素，添加.red
-//链式操作
-api.addClass('red')//this=api
+    // 遍历所有刚才获取的元素，添加.red
+    //链式操作
+    .addClass('red')//this=api
     .addClass('blue')
 
 obj.fn(p1)  //等价于下一个函数，函数里的this = obj
